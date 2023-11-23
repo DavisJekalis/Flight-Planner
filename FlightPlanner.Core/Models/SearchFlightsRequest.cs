2e4710c8220e89000b159632598f373924f6b2d4
DavisJekalis <jekalis.davis@gmail.com>
2023-11-23 15:55:14 +0200

    Update

--- FlightPlanner.Core/Models/SearchFlightsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlightPlanner.Models
-{
-    public class SearchFlightsRequest
-    {
-        public string From { get; set; }
-        public string to { get; set; }
-        public string departureDate { get; set; }
-    }
-}
